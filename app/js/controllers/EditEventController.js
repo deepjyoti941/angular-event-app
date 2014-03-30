b9b75c91d2abed10f0e5a77435864bd0ce1bfcb5
deepjyoti941 <deepjyoti941@gmail.com>
2014-03-31 01:13:55 +0530

    implementing two way binding by creating a new event form

---
+++ app/js/controllers/EditEventController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+eventsApp.controller('EditEventController', function ($scope) {
+
+	$scope.saveEvent = function (event) {
+		window.alert('event' + event.name + 'saved');
+	};
+
+	$scope.cancelEdit = function () {
+		window.location = "/EventDetails.html";
+	};
+});
