3a3f6efc1b0fbe8d9abd4891dade46a8976dd4cf
deepjyoti941 <deepjyoti941@gmail.com>
2014-03-31 00:26:37 +0530

    creating and using builtin filters

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,6 +2,7 @@
 
 eventsApp.controller('EventController', function EventController($scope) {
 
+	$scope.sortorder = 'name';
 	$scope.event = {
 		name: 'Angular Boot Camp',
 		date: '1/1/2013',
@@ -18,23 +19,33 @@ eventsApp.controller('EventController', function EventController($scope) {
 				creatorName: 'Bob smith',
 				duration: '1 hr',
 				level: 'Advanced',
-				abstract: 'In this session you will learn the ins and outs of directive!'
+				abstract: 'In this session you will learn the ins and outs of directive!',
+				upVoteCount:0
 			},
 			{
 				name: 'Scopes for fun and profit',
 				creatorName: 'jhon doe',
 				duration: '1 hr',
-				level: 'Biginner',
-				abstract: 'In this session you will learn the ins and outs of scopes!'
+				level: 'Introductory',
+				abstract: 'In this session you will learn the ins and outs of scopes!',
+				upVoteCount:0
 			},
 			{
 				name: "well behaved controller",
 				creatorName: 'jan doe',
 				duration: '1 hr',
 				level: 'Intermediate',
-				abstract: 'In this session you will learn the ins and outs of controllers!'
+				abstract: 'In this session you will learn the ins and outs of controllers!',
+				upVoteCount:0
 			}
 		]
 	}
+
+	$scope.upVoteSession = function (session) {
+		session.upVoteCount++;
+	};
 	
+	$scope.downVoteSession = function (session) {
+		session.upVoteCount--;
+	};
 });
