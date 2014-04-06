38b7c18dbd56b4f90d2648cf19ae69379552fb4b
deepjyoti941 <deepjyoti941@gmail.com>
2014-04-06 23:38:04 +0530

    accessing querystring parameter of the form ?foo=anything using $route.current.params.foo

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,7 +4,7 @@ eventsApp.controller('EventController',
     function EventController($scope, eventData, $routeParams, $route) {
         $scope.sortorder = 'name';
         $scope.event = eventData.getEvent($routeParams.eventId);
-        console.log($route.current.foo);
+        console.log($route.current.params.foo);
         //$scope.event = $route.current.locals.event;
 
         $scope.upVoteSession = function (session) {
