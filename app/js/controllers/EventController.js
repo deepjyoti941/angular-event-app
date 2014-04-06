56de805c3e7aa7a5444626f7ab4f88836624902d
deepjyoti941 <deepjyoti941@gmail.com>
2014-04-06 23:47:45 +0530

    reload the route to original without refreshing the page

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,8 +4,15 @@ eventsApp.controller('EventController',
     function EventController($scope, eventData, $routeParams, $route) {
         $scope.sortorder = 'name';
         $scope.event = eventData.getEvent($routeParams.eventId);
-        console.log($route.current.params.foo);
+        console.log($route.current.params.foo);    //params include everything i.e route properties and querystring properties
+        //console.log($route.current.pathParams.foo); //pathParams includes only routes properties
         //$scope.event = $route.current.locals.event;
+
+
+        /*reload the route to original without refreshing the page */
+        $scope.reload = function() {
+        	$route.reload();
+        }
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
