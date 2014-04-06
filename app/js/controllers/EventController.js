28d8e8c735fc6b1100ee051bae5fc50b006eb833
deepjyoti941 <deepjyoti941@gmail.com>
2014-04-06 23:34:25 +0530

    accessing custum property on route using $route.current

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,8 +3,9 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData, $routeParams, $route) {
         $scope.sortorder = 'name';
-
-        $scope.event = $route.current.locals.event;
+        $scope.event = eventData.getEvent($routeParams.eventId);
+        console.log($route.current.foo);
+        //$scope.event = $route.current.locals.event;
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
