c78e9d5976a461ae3db13657d168d12925b8f188
deepjyoti941 <deepjyoti941@gmail.com>
2014-04-10 15:03:21 +0530

    creating directive as an element

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,7 @@
 eventsApp.directive('mySample', function($compile) {
 	//return directive defination object
 	return {
-		link: function(scope, element, attrs, controller) {
-			var markup = "<input type='text' ng-model='sampleData' />{{sampleData}} <br/>";
-			angular.element(element).html($compile(markup)(scope));
-		}
-	};
+		restrict: 'E',
+		template: "<input type='text' ng-model='sampleData' />{{sampleData}} <br/>"
+	}
 });
