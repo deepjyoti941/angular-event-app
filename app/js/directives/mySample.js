74ff2c39e3bd29b37c3daa8681eea0e7b7baa2e1
deepjyoti941 <deepjyoti941@gmail.com>
2014-04-10 15:30:53 +0530

    creating isolate scope with different bindings on main scope

--- app/js/directives/mySample.js
+++ app/js/directives/mySample.js
@@ -5,8 +5,6 @@ eventsApp.directive('mySample', function($compile) {
 	return {
 		restrict: 'C',
 		template: "<input type='text' ng-model='sampleData' />{{sampleData}} <br/>",
-		scope: {
-			
-		}
+		scope: {}
 	}
 });
