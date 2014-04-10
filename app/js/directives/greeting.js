9c3c0be198b5442dbfc4216841268f435d3fafdb
deepjyoti941 <deepjyoti941@gmail.com>
2014-04-10 17:34:26 +0530

    controller inside directive way 1

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -4,6 +4,11 @@ eventsApp.directive('greeting', function($compile) {
 	return {
 		restrict: 'E',
 		replace: true,
-		template: ""
+		template: "<button class='btn' ng-click='sayHello()'>Say hello</button>",
+		controller: function($scope) {
+			$scope.sayHello = function() {
+				alert('hello');
+			}
+		}
 	}
 });
